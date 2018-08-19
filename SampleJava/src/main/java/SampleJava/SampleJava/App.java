a6153680e79c3aa247ec4bcedb40a63822c21437
tflucker <tflucker@gwmail.gwu.edu>
2018-08-19 17:13:58 -0400

    Sample

---
+++ SampleJava/src/main/java/SampleJava/SampleJava/App.java
@@ -0,0 +1,13 @@
+package SampleJava.SampleJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
