51a774f43959ee3acb4038a3d4f4654b0d5fb210
tflucker <tflucker@gwmail.gwu.edu>
2018-08-19 17:29:36 -0400

    First Commit

---
+++ JavaPractice/src/main/java/JavaPractice/JavaPractice/App.java
@@ -0,0 +1,13 @@
+package JavaPractice.JavaPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
